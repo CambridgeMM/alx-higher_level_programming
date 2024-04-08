7f846e03d2856fbcdb36941f8e7e341a3fada5bc
CambridgeMM <cambi.matome@gmail.com>
2024-04-08 08:29:40 -0700

    Arguments

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,4 +1,12 @@
 #!/usr/bin/node
-console.log('No argument');
-console.log('Arguments found');
-console.log('Arguments found');
+/ Check the number of arguments passed
+const numArgs = process.argv.length - 2; // Subtracting 2 to exclude 'node' and the script filename
+
+// Check the number of arguments and print the appropriate message
+if (numArgs === 0) {
+    console.log("No argument");
+} else if (numArgs === 1) {
+    console.log("Argument found");
+} else {
+    console.log("Arguments found");
+}
